f2a91768e662139307b0b49a00ed7d25f330dbc3
이준용 <iiiuooyoo@gmail.com>
2023-02-08 17:34:13 +0900

    feat : 아리 돌기

--- make-cats/pages/index.tsx
+++ make-cats/pages/index.tsx
@@ -1,5 +1,24 @@
 import axios from "axios";
+import styled from "@emotion/styled";
 import { useState } from "react";
+
+const ImageStyled = styled.img`
+    width: 100px;
+    height: 100px;
+
+    @keyframes spin {
+        from {
+            transform: rotate(0deg);
+        }
+        to {
+            transform: rotate(360deg);
+        }
+    }
+
+    margin: 5rem;
+
+    animation: spin infinite 3s linear;
+`;
 
 export default function Home() {
     const [src, setSrc] = useState("");
@@ -19,7 +38,8 @@ export default function Home() {
 
     return (
         <>
-            {src.length > 0 && <h1>김이슬 바보</h1>}
+            {src.length > 0 ? <h1>김이슬 바보</h1> : <h1>문구 삭제</h1>}
+            <ImageStyled src="ari2.jpg" />
             <p>누를 때 마다 생김 (안 나오면 좀 기다리셈)</p>
             <button onClick={callCats}>고양이 생성</button>
             <img src={src} />
