c7eff820956fdec5eb5a713e99923cc32472e2ee
이준용 <iiiuooyoo@gmail.com>
2023-02-09 15:45:30 +0900

    fix : createWrapper => useWrappedStore

--- make-cats/pages/_app.tsx
+++ make-cats/pages/_app.tsx
@@ -1,9 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import wrapper from "@/store";
+import { Provider } from "react-redux";
 
-function App({ Component, pageProps }: AppProps) {
-    return <Component {...pageProps} />;
+export default function App({ Component, ...pageProps }: AppProps) {
+    const { store, props } = wrapper.useWrappedStore(pageProps);
+
+    return (
+        <Provider store={store}>
+            <Component {...props.pageProps} />;
+        </Provider>
+    );
 }
-
-export default wrapper.withRedux(App);
