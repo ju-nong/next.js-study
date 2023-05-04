b4d7768531041ce4afdfca3f0c439b2f8a0f0667
이준용 <iiiuooyoo@gmail.com>
2023-05-04 14:35:58 +0900

    feat : Meta tag

--- make-cats/pages/_document.tsx
+++ make-cats/pages/_document.tsx
@@ -3,7 +3,27 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
     return (
         <Html lang="en">
-            <Head />
+            <Head>
+                <meta name="theme-color" content="#0f0f0f"></meta>
+                <meta
+                    name="description"
+                    content="여러 고양이를 만나보세요!"
+                ></meta>
+                <meta name="keywords" content="cat, Cat, CAT, 고양이"></meta>
+                <meta name="author" content="이준용"></meta>
+                <meta content="Make Cat" property="og:title"></meta>
+                <meta content="/favicon.jpg" property="og:image"></meta>
+                <meta content="website" property="og:type"></meta>
+                <meta
+                    content="make-cat.vercel.app"
+                    property="og:site_name"
+                ></meta>
+                <meta
+                    content="https://make-cat.vercel.app"
+                    property="og:type"
+                ></meta>
+                <link rel="icon" href="/favicon.jpg"></link>
+            </Head>
             <body>
                 <Main />
                 <NextScript />
